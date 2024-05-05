760e73d16d9c546d963295973642462067d80a01
Aniket Raut <rautaniket640@gmail.com>
2024-05-05 14:52:46 +0530

    File Structure Optimize

--- Main.js
+++ Main.js
@@ -3,14 +3,23 @@ $(function () {
     // Function to load header and footer
     function loadHeaderFooter() {
         // Load header
-        $.get("header.html", function (data) {
+        // $.get("header.html", function (data) {
+        //     $("#header").html(data);
+        // });
+
+        $.get("/Components/Header/header.html", function (data) {
             $("#header").html(data);
         });
         // Load footer
-        $.get("footer.html", function (data) {
+        // $.get("footer.html", function (data) {
+        //     $("#footer").html(data);
+        // });
+
+        $.get("/Components/Footer/footer.html", function (data) {
             $("#footer").html(data);
         });
     }
     // Call the function when the page loads
     loadHeaderFooter();
 });
+
