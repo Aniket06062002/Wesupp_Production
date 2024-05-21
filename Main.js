67747c686f6f37c6ab244f22b591c7773f9e85e8
Aniket Raut <rautaniket640@gmail.com>
2024-05-21 15:32:38 +0530

    pushing all changes

--- Main.js
+++ Main.js
@@ -7,9 +7,20 @@ $(function () {
             $("#header").html(data);
         });
        
-
         $.get("/Components/Footer/Footer.html", function (data) {
             $("#footer").html(data);
+        });
+
+        $.get("/Components/Vision/vision.html", function (data) {
+            $("#VisionMission").html(data);
+        });
+
+        $.get("/Components/Services/ServiseswithBtn.html", function (data) {
+            $("ServisesWithBtn").html(data);
+        });
+        
+        $.get("/Components/Services/ServiceComponent.html", function (data) {
+            $("ServiceComponent").html(data);
         });
     }
     // Call the function when the page loads
